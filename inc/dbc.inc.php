6dec876270369c3fd0cb38cb64bc726d2e8434c4
attoPascal <attoPascal@gmx.net>
2015-08-25 22:48:45 +0200

    Update dbc.inc.php

--- inc/dbc.inc.php
+++ inc/dbc.inc.php
@@ -1,6 +1,6 @@
 <?php
-  $db_connect = @mysql_connect('a1200595.mysql.univie.ac.at', 'a1200595', '1SecurePW')
+  $db_connect = @mysql_connect('REDACTED', 'REDACTED', 'REDACTED')
     or die("<div><b>ERROR:</b> Verbindung zur Datenbank fehlgeschlagen</div>");  
-  $db_select = @mysql_select_db('a1200595')
+  $db_select = @mysql_select_db('REDACTED')
     or die("<div><b>ERROR:</b> Auswahl der Datenbank fehlgeschlagen</div>");
 ?>
